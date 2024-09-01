234aeb5c0ed6b953518e1903c3343bcd4894569e
Sidnei Filho <sialkas@gmail.com>
2024-09-01 11:09:02 -0300

    feat(Aula 2): :zap: update

--- ms-consolidacao/internal/infra/kafka/factory/factory.go
+++ ms-consolidacao/internal/infra/kafka/factory/factory.go
@@ -5,19 +5,19 @@ import "github.com/sialka/cartola_fc/internal/infra/kafka/event"
 func CreateProcessMessageStrategy(topic string) event.ProcessEventStrategy {
 	switch topic {
 	case "chooseTeam":
-		// JSON do Topico: {"my_team_id":"1", "players":["1","2","3","4","5"]}
+		// Envio no Tópico: {"my_team_id":"1", "players":["1","2","3","4","5"]}
 		return event.ProcessChooseTeam{}
 	case "newPlayer":
-		// JSON do Topico: {"id": "10","name": "Wesley","initial_price": 10.5}
+		// Envio no Tópico: {"id": "10","name": "Wesley","initial_price": 10.5}
 		return event.ProcessNewPlayer{}
 	case "newMatch":
-		// JSON do Topico: {"id":"3","match_date":"2021-05-01T00:00:00Z","team_a_id":"1","team_b_id":"2"}
+		// Envio no Tópico: {"id":"3","match_date":"2021-05-01T00:00:00Z","team_a_id":"1","team_b_id":"2"}
 		return event.ProcessNewMatch{}
 	case "newAction":
-		// JSON do Topico: {"match_id":"3","team_id":"1","player_id":"1","action":"goal","minutes":10}
+		// Envio no Tópico: {"match_id":"3","team_id":"1","player_id":"1","action":"goal","minutes":10}
 		return event.ProcessNewAction{}
 	case "matchUpdateResult":
-		// JSON do Topico: {"match_id":"1","result":"2-0"}
+		// Envio no Tópico: {"match_id":"1","result":"2-0"}
 		return event.ProcessMatchUpdateResult{}
 	}
 	return nil
